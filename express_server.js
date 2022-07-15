d258d4ee48859d16647ae82d8a69358401bc7380
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-07-14 19:47:44 -0600

    adding cookies and rearrangin code

--- express_server.js
+++ express_server.js
@@ -2,28 +2,38 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
 
+//constants
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
 //GET
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get('/urls/new', (req, res)=>{
-  res.render('urls_new')
+  let templateVars = {username: req.cookies["username"]}
+  res.render('urls_new', templateVars)
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -33,6 +43,7 @@ app.get("/u/:id", (req, res) => {
 });
 
 //post
+
 app.post('/urls/:id/delete', (req, res)=>{
   delete urlDatabase[req.params.id]
   res.redirect('/urls')
@@ -50,6 +61,17 @@ app.post('/urls/:id/modify', (req, res)=>{
   urlDatabase[url] = newURL
   res.redirect('/urls')
 })
+
+app.post('/login',(req,res)=>{
+  res.cookie('username', req.body.username)
+  res.redirect('/urls')
+})
+
+app.post('/logout',(req,res)=>{
+  res.clearCookie('username')
+  res.redirect('/urls')
+})
+
 
 
 app.listen(PORT, () => {
